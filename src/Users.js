71acf35b9c0e9ca33e653ca57f4dd00fcbee650f
manivasagam <s.kishore123.64@gmail.com>
2022-10-06 18:47:05 +0530

    first Commit

---
+++ src/Users.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Users() {
+  return (
+    <img src='https://4kwallpapers.com/images/walls/thumbs/8375.jpg' width="500px" height={300}></img>
+  )
+}
+
+export default Users
