598d36bf9fe7b553666feab3fcd07c6602c14b26
manivasagam <s.kishore123.64@gmail.com>
2022-10-12 23:37:23 +0530

    form validation done

--- src/User_create.js
+++ src/User_create.js
@@ -1,63 +1,169 @@
+import axios from 'axios';
+import { useFormik } from 'formik'
 import React from 'react'
 
 function User_create() {
+    const formik = useFormik({
+        initialValues: {
+            name: '',
+            email: '',
+            country: '',
+            state: '',
+            city: '',
+            phone: '',
+            DOB: '',
+            gender: '',
+
+        },
+        validate: (values) => {
+            let error = {};
+
+            if (!values.name) {
+                error.name = "please enter the Name"
+            }
+            if ((values.name) && (values.name.length <= 2 || values.name.length > 15)) {
+                error.name = "Username must be between 3 to 15"
+            }
+            if (!values.email) {
+                error.email = 'please enter Email'
+            }
+            if ((values.email) && (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email))) {
+                error.email = "please enter a valid Email"
+            }
+
+            if (!values.phone) {
+                error.phone = 'please enter your Phone Number'
+            }
+            if (values.phone.toString().length !== 10) {
+                error.phone = 'please enter a valid Phone Number'
+            }
+            // console.log(values.DOB);
+            // console.log(values.DOB.split('-'));
+            console.log(values.DOB.split('-')[0]);
+            console.log(new Date().getFullYear());
+            let age = new Date().getFullYear() - +values.DOB.split('-')[0];
+            console.log(age);
+            if (age < 18) {
+                error.DOB = "you must be above 18"
+            }
+            return error;
+        },
+        onSubmit: async (values) => {
+            try {
+                console.log(values);
+                const userdata = await axios.post('https://63450500dcae733e8fe7f213.mockapi.io/users', values)
+                alert('Success')
+            }
+            catch (error) {
+                alert("Error")
+            }
+        },
+
+    });
     return (
         <div className='container'>
-            <div className="row">
-                <div className='col-lg-6'>
-                    <div className='form-group'><lable>Name</lable><input className='form-control' type={"text"} /></div>
-                </div>
-                <div className='col-lg-6'>
-                    <div className='form-group'><lable>Email</lable><input className='form-control' type={"email"} /></div>
-                </div>
-                <div className='col-lg-4'>
-                    <div className='form-group'><lable>Country</lable>
-                        <select className='form-control'>
-                            <option>INDIA</option>
-                            <option>US</option>
-                            <option>CHINA</option>
-                        </select>
+            <form onSubmit={formik.handleSubmit}>
+                <div className="row">
+                    <div className='col-lg-6'>
+                        <div className='form-group'>
+                            <lable>Name</lable>
+                            <input name="name"
+                                onChange={formik.handleChange}
+                                value={formik.values.name}
+                                onBlur={formik.handleBlur}
+                                className={`form-control ${formik.touched.name && formik.errors.name ? "error-box" : ""} ${formik.touched.name && !formik.errors.name ? "success-box" : ''}`}
+
+                                type={"text"} />
+                            {
+                                formik.touched.name && formik.errors.name ? <span style={{ color: "red" }}>{formik.errors.name}</span> : null
+                            }
+                        </div>
                     </div>
-                </div>
-                <div className='col-lg-4'>
-                    <div className='form-group'><lable>State</lable>
-                        <select className='form-control'>
-                            <option>TN</option>
-                            <option>MH</option>
-                            <option>PN</option>
-                        </select>
+                    <div className='col-lg-6'>
+                        <div className='form-group'><lable>Email</lable>
+                            <input name='email'
+                                onChange={formik.handleChange}
+                                value={formik.values.email}
+                                onBlur={formik.handleBlur}
+                                className={`form-control ${formik.touched.email && formik.errors.email ? "error-box" : ""} ${formik.touched.email && !formik.errors.email ? "success-box" : ''}`}
+                                type={"email"} />
+                            {
+                                formik.touched.email&&formik.errors.email ? <span style={{ color: "red" }}>{formik.errors.email}</span> : null
+                            }
+                        </div>
                     </div>
-                </div>
-                <div className='col-lg-4'>
-                    <div className='form-group'><lable>City</lable>
-                        <select className='form-control'>
-                            <option>MADURAI</option>
-                            <option>CHENNAI</option>
-                            <option>COIMBATORE</option>
-                        </select>
+                    <div className='col-lg-4'>
+                        <div className='form-group' name='country' onChange={formik.handleChange} value={formik.values.country}><lable>Country</lable>
+                            <select className='form-control'>
+                                <option>INDIA</option>
+                                <option>US</option>
+                                <option>CHINA</option>
+                            </select>
+                        </div>
                     </div>
+                    <div className='col-lg-4'>
+                        <div className='form-group'><lable>State</lable>
+                            <select name='state' onChange={formik.handleChange} value={formik.values.state} className='form-control'>
+                                <option>TN</option>
+                                <option>MH</option>
+                                <option>PN</option>
+                            </select>
+                        </div>
+                    </div>
+                    <div className='col-lg-4'>
+                        <div className='form-group'><lable>City</lable>
+                            <select name='city' onChange={formik.handleChange} value={formik.values.city} className='form-control'>
+                                <option>MADURAI</option>
+                                <option>CHENNAI</option>
+                                <option>COIMBATORE</option>
+                            </select>
+                        </div>
+                    </div>
+                    <div className='col-lg-4'>
+                        <div className='form-group'>
+                            <lable>PhoneNo</lable>
+                            <input name='phone'
+                                onChange={formik.handleChange}
+                                value={formik.values.phone}
+                                onBlur={formik.handleBlur}
+                                className={`form-control ${formik.touched.phone && formik.errors.phone ? "error-box" : ""} ${formik.touched.phone && !formik.errors.phone ? "success-box" : ''}`}
+                                type={"number"} />
+                            {
+                                formik.touched.phone&&formik.errors.phone ? <span style={{ color: "red" }}>{formik.errors.phone}</span> : null
+                            }
+                        </div>
+                    </div>
+                    <div className='col-lg-4'>
+                        <div className='form-group'>
+                            <lable>DOB</lable>
+                            <input name='DOB'
+                                onChange={formik.handleChange}
+                                value={formik.values.DOB}
+                                onBlur={formik.handleBlur}
+                                max={"2004-01-01"}
+                                className={`form-control ${formik.touched.DOB && formik.errors.DOB ? "error-box" : ""} ${formik.touched.DOB && !formik.errors.DOB ? "success-box" : ''}`}
+                                type={"date"} />
+                            {
+                                formik.touched.DOB&&formik.errors.DOB ? <span style={{ color: "red" }}>{formik.errors.DOB}</span> : null
+                            }
+                        </div>
+                    </div>
+                    <div className='col-lg-4'>
+                        <div className='form-group'><lable>Gender</lable>
+                            <select name='gender' onChange={formik.handleChange} value={formik.values.gender} className='form-control'>
+                                <option>female</option>
+                                <option>male</option>
+                            </select>
+                        </div>
+                    </div>
+                    <div className='col-lg-4'>
+                        <div className='form-group'>
+                            <input className='btn btn-primary' type={"submit"} />
+                        </div>
+                    </div>
+
                 </div>
-                <div className='col-lg-4'>
-                <div className='form-group'><lable>PhoneNo</lable><input className='form-control' type={"number"} /></div>   
-                </div>
-                <div className='col-lg-4'>
-                <div className='form-group'><lable>DOB</lable><input className='form-control' type={"date"} /></div>
-                </div>
-                <div className='col-lg-4'>
-                <div className='form-group'><lable>Gender</lable>
-                <select className='form-control'>
-                            <option>female</option>
-                            <option>male</option>
-                        </select>
-                </div>
-                </div>
-                <div className='col-lg-4'>
-                <div className='form-group'>
-                    <input className='btn btn-primary' type={"submit"} />
-                    </div>   
-                </div>
-                
-            </div>
+            </form>
         </div >
     )
 }
--- src/Users.js
+++ src/Users.js
@@ -1,50 +1,45 @@
+import axios from 'axios';
 import React, { useEffect, useState } from 'react'
 import { Link } from 'react-router-dom'
 function Users() {
-    const [users,setUser]=useState([]);
-    useEffect(()=>{
-        setUser([
-            {
-                id : 1,
-                name: "Mani",
-                email : "123@gmail.com",
-                country : "India",
-                state : "TamilNadu",
-                city : "Madurai",
-                phone : "1234567890",
-                DOB : "12/10/1998",
-                gender : "male"
-            },
-            {
-                id : 2,
-                name: "Kishore",
-                email : "456@gmail.com",
-                country : "India",
-                state : "TamilNadu",
-                city : "chennai",
-                phone : "1234567890",
-                DOB : "12/10/1998",
-                gender : "male"
-            }
-        ])
-    },[])
-  return (
-    <div class="container-fluid"> 
-    <div class="d-sm-flex align-items-center justify-content-between mb-4">
+    const [users, setUser] = useState([]);
+    const [isLoading, setIsLoading] = useState(false)
+    useEffect(() => {
+        fetchdata();
+
+    }, [])
+    let fetchdata = async () => {
+        try {
+            setIsLoading(true);
+            let userdata = await axios.get("https://63450500dcae733e8fe7f213.mockapi.io/users")
+            console.log(userdata);
+            setUser(userdata.data);
+            setIsLoading(false);
+        } catch (error) {
+            alert('Error')
+        }
+    }
+    return (
+        <div class="container-fluid">
+            <div class="d-sm-flex align-items-center justify-content-between mb-4">
                 <h1 class="h3 mb-0 text-gray-800">Users</h1>
                 <Link to="/User_create" class="d-none d-sm-inline-block btn btn-sm btn-primary shadow-sm"><i
                     class="fas fa-download fa-sm text-white-50"></i> Create Users</Link>
             </div>
-                            <div class="card shadow mb-4">
-                            <div class="card-header py-3">
+            {
+                isLoading ? <div class="spinner-border" role="status">
+                    <span class="visually-hidden">Loading...</span>
+                </div>
+                    : <div class="card shadow mb-4">
+                        <div class="card-header py-3">
                             <h6 class="m-0 font-weight-bold text-primary">DataTables Example</h6>
-                            </div>
-                            <div class="card-body">
+                        </div>
+                        <div class="card-body">
                             <div class="table-responsive">
-                            <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
-                            <thead>
+                                <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
+                                    <thead>
                                         <tr>
-                                        <th>ID</th>
+                                            <th>ID</th>
                                             <th>Name</th>
                                             <th>Email</th>
                                             <th>Country</th>
@@ -58,8 +53,8 @@ function Users() {
                                     </thead>
                                     <tfoot>
                                         <tr>
-                                        <th>ID</th>
-                                        <th>Name</th>
+                                            <th>ID</th>
+                                            <th>Name</th>
                                             <th>Email</th>
                                             <th>Country</th>
                                             <th>State</th>
@@ -71,32 +66,34 @@ function Users() {
                                         </tr>
                                     </tfoot>
                                     <tbody>
-                                   {users.map((user)=>{
-                                    return <tr>
-                                    <td>{user.id}</td>
-                                            <td>{user.name}</td>
-                                            <td>{user.email}</td>
-                                            <td>{user.country}</td>
-                                            <td>{user.state}</td>
-                                            <td>{user.city}</td>
-                                            <td>{user.phone}</td>
-                                            <td>{user.DOB}</td>
-                                            <td>{user.gender}</td>
-                                            <td>
-                                                <Link className='btn btn-primary' to={`/user/${user.id}`}>View User</Link>
-                                                <Link className='btn btn-primary mt-1' to={`/edituser/${user.id}`}>Edit User</Link>
-                                            </td>
-                                        </tr>
-                                  
-                                })}    
-                                        
+                                        {users.map((user) => {
+                                            return <tr>
+                                                <td>{user.id}</td>
+                                                <td>{user.name}</td>
+                                                <td>{user.email}</td>
+                                                <td>{user.country}</td>
+                                                <td>{user.state}</td>
+                                                <td>{user.city}</td>
+                                                <td>{user.phone}</td>
+                                                <td>{user.DOB}</td>
+                                                <td>{user.gender}</td>
+                                                <td>
+                                                    <Link className='btn btn-primary' to={`/user/${user.id}`}>View User</Link>
+                                                    <Link className='btn btn-primary mt-1' to={`/edituser/${user.id}`}>Edit User</Link>
+                                                </td>
+                                            </tr>
+
+                                        })}
+
                                     </tbody>
-                            </table>
+                                </table>
                             </div>
-                            </div>
-                            </div>
-    </div>
-  )
+                        </div>
+                    </div>
+
+            }
+        </div>
+    )
 }
 
 export default Users
